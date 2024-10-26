6714b25188bb98d95c157090ac60459b4b0e2209
Ivan Bolshakov <idbolshakov@gmail.com>
2024-10-26 11:58:34 +0500

    feat: implement read file & parse yaml content features

--- main.go
+++ main.go
@@ -1,3 +1,34 @@
 package main
 
-func main() {}
+import (
+  "os"
+  "fmt"
+  "gopkg.in/yaml.v3"
+)
+
+func main() {
+  // 0: read file
+  content, err := os.ReadFile("pod.yaml")
+  if err != nil {
+    fmt.Println(fmt.Errorf("cannot read file content: %w", err))
+    os.Exit(-1)
+    return
+  }
+  print("fff")
+
+  // 1: parse yaml
+  var root yaml.Node
+  if err := yaml.Unmarshal(content, &root); err != nil {
+    fmt.Println(fmt.Errorf("cannot unmarshal file content: %w", err))
+    os.Exit(-1)
+    return
+  }
+
+
+  for _, doc := range root.Content {
+    // поле `.Content` содержит вложенные дочерние элементы
+    for _, node := range doc.Content {
+      fmt.Printf("line: %d, value: %s\n", node.Line, node.Value)
+    }
+  } 
+}
